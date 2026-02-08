4320334cb3601c2101662bfcac95eb0cbe1f7bab
pblo <pdiazarenas541@gmail.com>
2026-02-08 23:09:13 +0000

    FEAT/ index.js

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 import express from 'express'
 import cors from 'cors'
+import dotenv from 'dotenv'
+dotenv.config()
+
+
+
+const app = express()
+const PORT = process.env.API_PORT || 3001
+
+app.use(express.json())
+app.use(cors())
+
+
+app.get('/', (req,res)=>{
+    res.send("PAGE OK")
+})
+
+
+app.listen(PORT, async() =>{
+    console.log(`Server running on http://localhost:${PORT}`)
+})

caaa0fe4a795a5e514df3d3b843ad79c499e6aa0
pblo <pdiazarenas541@gmail.com>
2026-02-08 23:52:21 +0000

    FEAT/ inventario.controllers.js

---
+++ controllers/inventario.controllers.js
@@ -0,0 +1,35 @@
+import inventarioModel from "../models/inventario.models";
+
+const readAllLimit = async (req,res) =>{
+    const queryStrings = req.query
+    try {
+        const joyas = await inventarioModel.getAllJoyasLimit(queryStrings)
+
+        if(!joyas){
+            return res.status(404).json({message: 'Requeste Element(s) Not Found'})
+        }
+    } catch (error) {
+        return res.status(500).json({message: error.message})
+    }
+
+}
+
+const readFilter = async (req,res) =>{
+    const queryStrings = req.query
+try {
+    const joyas = await inventarioModel.getFilteredJoyas(queryStrings)
+    
+        if(!joyas){
+            return res.status(404).json({message: 'Requested element(s) Not Found'})
+        }
+    } catch (error) {
+        return res.status(500).json({message: error.message})
+    }
+}
+
+const inventarioController = {
+    readAllLimit,
+    readFilter,
+}
+
+export default inventarioController

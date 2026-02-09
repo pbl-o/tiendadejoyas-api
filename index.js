d48504dbe838c6a0ea7607ec9f711b778432370f
pblo <pdiazarenas541@gmail.com>
2026-02-09 00:02:32 +0000

    FEAT/ routes + edit index.js + add middleware (reqRegister) 2

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
-dotenv.config()
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+import router from "./routes/inventario.routes";
+import { getDate } from "./database/database";
+import fs from "node:fs/promises";
 
+export const reqRegister = async (req, res, next) => {
+  try {
+    const method = req.method;
+    const path = req.path;
+    const register = `Request received: ${method} ${path}`;
+    await fs.appendFile("log.txt", register, "\n");
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
 
+const app = express();
+const PORT = process.env.API_PORT || 3001;
 
-const app = express()
-const PORT = process.env.API_PORT || 3001
+app.use(express.json());
+app.use(cors());
+app.use(reqRegister);
+app.use("/joyas", router);
 
-app.use(express.json())
-app.use(cors())
+app.get("/", (req, res) => {
+  res.send("PAGE OK");
+});
 
-
-app.get('/', (req,res)=>{
-    res.send("PAGE OK")
-})
-
-
-app.listen(PORT, async() =>{
-    console.log(`Server running on http://localhost:${PORT}`)
-})
+app.listen(PORT, async () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+  await getDate();
+});
